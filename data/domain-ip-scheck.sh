20e3878e650bf00321fd18b7c22c6bb7b8f0325b
Haecky <96848760+Haecky@users.noreply.github.com>
2023-02-25 21:08:02 +0100

    Update domain-ip-scheck.sh
    DNS_SERVER variabel

--- data/domain-ip-scheck.sh
+++ data/domain-ip-scheck.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # set -e
 DATUM=$(date +%Y-%m-%d\ %H:%M:%S)
+DNS_SERVER=${DNSSERVER}
 if ! curl -4sf --user-agent "${CURL_USER_AGENT}" "https://ipv64.net" 2>&1 > /dev/null; then
     echo "$DATUM  FEHLER !!!  - 404 Sie haben kein Netzwerk oder Internetzugang oder die Webseite ipv64.net ist nicht erreichbar"
     echo "=============================================================================================="
@@ -17,11 +18,11 @@ fi
 
 if [[ "${DOMAIN_PRAEFIX_YES}" =~ (YES|yes|Yes) ]] ; then
     for DOMAIN in $(echo "${DOMAIN_IPV64}" | sed -e "s/,/ /g"); do
-        echo "`date +%Y-%m-%d\ %H:%M:%S`  IP CHECK    - Deine DOMAIN mit PRAEFIX ${DOMAIN_PRAEFIX}.${DOMAIN} HAT DIE IP=`dig +short ${DOMAIN_PRAEFIX}.${DOMAIN} A @ns1.ipv64.net`" >> /var/log/cron.log 2>&1
+        echo "`date +%Y-%m-%d\ %H:%M:%S`  IP CHECK    - Deine DOMAIN mit PRAEFIX ${DOMAIN_PRAEFIX}.${DOMAIN} HAT DIE IP=`dig +short ${DOMAIN_PRAEFIX}.${DOMAIN} A @$DNS_SERVER`" >> /var/log/cron.log 2>&1
     done
 else
     for DOMAIN in $(echo "${DOMAIN_IPV64}" | sed -e "s/,/ /g"); do
-        echo "`date +%Y-%m-%d\ %H:%M:%S`  IP CHECK    - Deine DOMAIN ${DOMAIN} HAT DIE IP=`dig +short ${DOMAIN} A @ns1.ipv64.net`" >> /var/log/cron.log 2>&1
+        echo "`date +%Y-%m-%d\ %H:%M:%S`  IP CHECK    - Deine DOMAIN ${DOMAIN} HAT DIE IP=`dig +short ${DOMAIN} A @$DNS_SERVER`" >> /var/log/cron.log 2>&1
     done
 fi
 echo "=============================================================================================="
